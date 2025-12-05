e78bc8ca4b977e572b101f389c09849a8d3f91d0
Mitchell Kottke <mkottke2000@gmail.com>
2025-12-04 20:58:14 -0600

    2025 Day 3 part 2

--- 25/d3/d3.go
+++ 25/d3/d3.go
@@ -25,47 +25,34 @@ func getBanks(file_path string) []string {
 	return banks
 }
 
-func part1(banks []string) {
-	total := 0
+func getMaxJolts(banks []string, num_digits int) int64 {
+	total := int64(0)
 
 	for _, bank := range banks {
-		first_ind := 0
-		second_ind := 0
-		max_jolt := '0'
+		last_ind := -1
+		bank_jolts_str := ""
 
-		for battery_ind := 0; battery_ind < len(bank)-1; battery_ind++ {
-			battery_jolt := rune(bank[battery_ind])
-			if battery_jolt <= max_jolt {
-				continue
+		for curr_diget := 1; curr_diget <= num_digits; curr_diget++ {
+			max_jolt := '0'
+
+			for battery_ind := last_ind + 1; battery_ind < len(bank)-num_digits+curr_diget; battery_ind++ {
+				battery_jolt := rune(bank[battery_ind])
+				if battery_jolt <= max_jolt {
+					continue
+				}
+
+				max_jolt = battery_jolt
+				last_ind = battery_ind
+
+				if max_jolt == '9' {
+					break
+				}
 			}
 
-			max_jolt = battery_jolt
-			first_ind = battery_ind
-
-			if battery_jolt == '9' {
-				break
-			}
+			bank_jolts_str += bank[last_ind : last_ind+1]
 		}
 
-		max_jolt = '0'
-
-		for battery_ind := first_ind + 1; battery_ind < len(bank); battery_ind++ {
-			battery_jolt := rune(bank[battery_ind])
-			if battery_jolt <= max_jolt {
-				continue
-			}
-
-			max_jolt = battery_jolt
-			second_ind = battery_ind
-
-			if max_jolt == '9' {
-				break
-			}
-		}
-
-		bank_jolts_str := bank[first_ind:first_ind+1] + bank[second_ind:second_ind+1]
-		fmt.Printf("For bank %s got %s jolts\n", bank, bank_jolts_str)
-		bank_jolts, err := strconv.Atoi(bank_jolts_str)
+		bank_jolts, err := strconv.ParseInt(bank_jolts_str, 10, 64)
 		if err != nil {
 			fmt.Printf("Failed to convert string to number: %s", bank_jolts_str)
 			os.Exit(1)
@@ -73,11 +60,22 @@ func part1(banks []string) {
 		total += bank_jolts
 	}
 
-	fmt.Printf("Total jolts: %d\n", total)
+	return total
+}
+
+// Answer from my test set: 16973
+func part1(banks []string) {
+	fmt.Printf("Part 1 jolts: %d\n", getMaxJolts(banks, 2))
+}
+
+// Answer from my test set: 168027167146027
+func part2(banks []string) {
+	fmt.Printf("Part 2 jolts: %d\n", getMaxJolts(banks, 12))
 }
 
 func main() {
 	banks := getBanks("test.txt")
 
 	part1(banks)
+	part2(banks)
 }
